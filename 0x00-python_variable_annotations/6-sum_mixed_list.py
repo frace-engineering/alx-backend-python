fe98b7b197587f325654031ed26606612c1e677a
Engr Ugwu <fugwungene@gmail.com>
2024-03-17 13:52:16 +0000

    Reposition the import statement.

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-from typing import List, Union
 """Craete a type annotated function that sums a list of mixed variable types
 """
+from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usssr/bin/env python3
-from typing import Union, Tuple
 """Create a type annotated function that takes twoargs of type string and
 integer or float and return a tuple of type str and float..
 """
+from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
