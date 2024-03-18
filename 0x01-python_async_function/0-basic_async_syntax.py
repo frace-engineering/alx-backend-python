04ca380517067f3dc0693349a20176f6608d00b0
Engr Ugwu <fugwungene@gmail.com>
2024-03-18 21:59:30 +0000

    Fix bug in code.
    Add union of int and float as return type.

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,9 +5,10 @@ Demonstrating async coroutine using random module.
 """
 import asyncio
 import random
+from typing import Union
 
 
-async def wait_random(max_delay: int = 10) -> int:
+async def wait_random(max_delay: int = 10) -> Union[int, float]:
     """
     Asyncronous coroutine that waits for randome number of
     seconds from 0 - 10, then return the number.
